80f8dc4fea93ec89ccd86d2a5e8cdd34af445487
stocaimaza <stocaimaza@hotmail.com>
2023-12-18 22:42:42 -0300

    clase 2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
+
+const App = () => {
+  return (
+    <div>
+      <ItemListContainer saludo = "Hola Comisión, los quiero mucho" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+  return (
+    <h2> {props.saludo} </h2>
+  )
+}
+
+export default ItemListContainer
